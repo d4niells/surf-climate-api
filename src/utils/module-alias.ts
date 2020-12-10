39815880b37f863e0e6e26de9cfa60bde2443e1e
Daniel Mobills <daniel@mobills.com>
2020-12-09 23:12:50 -0300

    [ADD]: Run functional tests forecast controller

--- src/utils/module-alias.ts
+++ src/utils/module-alias.ts
@@ -3,8 +3,7 @@ import moduleAlias from 'module-alias';
 
 const files = path.resolve(__dirname, '../..');
 
-// create alias for absolute paths with nodejs + typescript 
 moduleAlias.addAliases({
   '@src': path.join(files, 'src'),
-  '@test': path.join(files, 'test')
-})
+  '@test': path.join(files, 'test'),
+});
---
+++ test/globals.d.ts
@@ -0,0 +1,6 @@
+
+declare namespace NodeJS {
+  interface Global {
+    testRequest: import('supertest').SuperTest<import('supertest').Test>
+  }
+}
