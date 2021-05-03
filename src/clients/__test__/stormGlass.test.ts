bb9fba0765c323e061884ce9e1f5b6bbc5784fe8
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-05-02 21:22:03 -0300

    test: checks case of error of limit of requests to the StormGlass API

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -56,4 +56,22 @@ describe('StormGlass client', () => {
       'Unexpected error when trying to communicate to StormGlass: Network Error'
     );
   });
+
+  it('Should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { erroes: ['Rate limit reached'] },
+      },
+    });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error returned by the StormGlass service: Error: {"erroes":["Rate limit reached"]} Code: 429'
+    );
+  });
 });
