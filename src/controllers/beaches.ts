4c21588d4a9a35aa4745bef6609975b4ef63a8a7
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-05-08 22:52:58 -0300

    feat: create beach on database and return created beach

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,13 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
+import { Beach } from '@src/models/beaches';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(request: Request, response: Response): Promise<void> {
-    response.status(201).send({ ...request.body, id: 'fake-id' });
+    const beach = new Beach(request.body);
+    const result = await beach.save();
+    response.status(201).send(result);
   }
 }
