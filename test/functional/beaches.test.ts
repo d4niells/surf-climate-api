98935fa08cbe9ea6882df0b037cdc15ac286b061
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-05-08 23:21:19 -0300

    test: check if return status 422 when is mongoose validation error

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -18,5 +18,26 @@ describe('Beaches functional tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
+
+    it('Should return status 422 when is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+      });
+    });
+
+    it.skip('Should return status 500 when there is any error than validation error', async () => {
+      // validate status 500 (Internal Server Error)
+    });
   });
 });
