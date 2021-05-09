daa001a9bd9d1a4ca3346ad31c03b1786fa6b1cf
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-05-08 23:23:26 -0300

    chore: return status 422 if it's a validation error

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,13 +1,22 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { Beach } from '@src/models/beaches';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(request: Request, response: Response): Promise<void> {
-    const beach = new Beach(request.body);
-    const result = await beach.save();
-    response.status(201).send(result);
+    try {
+      const beach = new Beach(request.body);
+      const result = await beach.save();
+      response.status(201).send(result);
+    } catch (err) {
+      if (err instanceof mongoose.Error.ValidationError) {
+        response.status(422).send({ error: err.message });
+      } else {
+        response.status(500).send({ error: 'Internal Server Error ' });
+      }
+    }
   }
 }
