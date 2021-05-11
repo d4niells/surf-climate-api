28c87c0a6c39c8b3f3269770236c249da4774729
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-05-10 23:57:46 -0300

    feat: initialize server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello word');
+import { SetupServer } from './server';
+import config from 'config';
+
+// Self invocked function
+(async (): Promise<void> => {
+  const server = new SetupServer(config.get('App.port'));
+  await server.init();
+  server.start();
+})();
