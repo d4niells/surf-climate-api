46c4bdd15d0f45f15802404160ef1086270ae02e
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-05-16 14:12:07 -0300

    refactor(validation-errors): improve client error validation

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,15 +1,32 @@
 import { Response } from 'express';
 import mongoose from 'mongoose';
+import { CUSTOM_VALIDATION } from '@src/models/user';
 
+interface Error {
+  code: number;
+  error: string;
+}
 export abstract class BaseController {
-  protected sendCreatedUpdateErrorResponse(
+  protected sendCreateUpdateErrorResponse(
     response: Response,
     error: mongoose.Error.ValidationError | Error
   ): void {
     if (error instanceof mongoose.Error.ValidationError) {
-      response.status(422).send({ code: 422, error: error.message });
+      const clientErrors = this.handleClientErros(error);
+      response.status(clientErrors.code).send(clientErrors);
     } else {
       response.status(500).send({ code: 500, error: 'Something went wronng!' });
     }
+  }
+
+  private handleClientErros(error: mongoose.Error.ValidationError): Error {
+    const duplicatedKindErrors = Object.values(error.errors).filter(
+      (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
+    );
+
+    if (duplicatedKindErrors.length) {
+      return { code: 409, error: error.message };
+    }
+    return { code: 422, error: error.message };
   }
 }
