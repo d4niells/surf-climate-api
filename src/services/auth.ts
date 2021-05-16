9ce35a0ad62ab1856bfdb9600521c8ad267d87c7
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-05-16 19:39:45 -0300

    feat: create a method to generate a token for authenticated users

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export class Auth {
   public static async hashPassword(
@@ -13,5 +15,15 @@ export class Auth {
     hashPassword: string
   ): Promise<boolean> {
     return await bcrypt.compare(password, hashPassword);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  public static generateToken(payload: object): string {
+    const tokenSecretKey: string = config.get('App.auth.tokenSecretKey');
+    const tokenExpiresIn: string = config.get('App.auth.tokenExpiresIn');
+
+    return jwt.sign(payload, tokenSecretKey, {
+      expiresIn: tokenExpiresIn,
+    });
   }
 }
