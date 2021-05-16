f212520d3cf233f932d4c22c8e0abb856af81d2d
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-05-16 19:38:29 -0300

    feat: create a method to autenticate users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,7 @@ import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from '@src/controllers';
+import { Auth } from '@src/services/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -14,5 +15,28 @@ export class UsersController extends BaseController {
     } catch (error) {
       this.sendCreateUpdateErrorResponse(response, error);
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(
+    request: Request,
+    response: Response
+  ): Promise<Response | undefined> {
+    const { email, password } = request.body;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user)
+      return response.status(401).send({ code: 401, error: 'User not found!' });
+
+    const validPassword = await Auth.comparePasswords(password, user.password);
+    if (!validPassword)
+      return response
+        .status(401)
+        .send({ code: 401, error: 'Password does not match!' });
+
+    const token = Auth.generateToken(user.toJSON());
+
+    return response.status(200).send({ token: token });
   }
 }
