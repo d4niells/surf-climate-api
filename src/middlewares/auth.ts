f53b782f1082cfcd10601e9df8c68eaf5f3c2076
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-05-20 22:48:44 -0300

    chore(auth): create meddleware to decode user token

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,18 @@
+import { Auth } from '@src/services/auth';
+import { NextFunction, Request, Response } from 'express';
+
+export const authMiddleware = (
+  request: Partial<Request>,
+  response: Partial<Response>,
+  next: NextFunction
+): void => {
+  try {
+    const token = request.headers?.['x-access-token'];
+    const decoded = Auth.decodeToken(token as string);
+
+    request.decoded = decoded;
+    next();
+  } catch (err) {
+    response.status?.(401).send({ code: 401, error: err.message });
+  }
+};
