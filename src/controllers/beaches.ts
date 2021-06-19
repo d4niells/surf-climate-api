309b3bc4980d5b34c18ab7ac8f2b10b61279b228
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-06-19 10:31:21 -0300

    feat: add auth middleware on method create a new Beach

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,14 +1,16 @@
-import { Controller, Post } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
-import { Beach } from '@src/models/beaches';
 import mongoose from 'mongoose';
+import { Beach } from '@src/models/beaches';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('beaches')
+@ClassMiddleware(authMiddleware)
 export class BeachesController {
   @Post('')
   public async create(request: Request, response: Response): Promise<void> {
     try {
-      const beach = new Beach(request.body);
+      const beach = new Beach({ ...request.body, user: request.decoded?.id });
       const result = await beach.save();
       response.status(201).send(result);
     } catch (err) {
