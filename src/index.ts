c030fa6b9e7dd72c8d21f87c359e2ede800cfa8a
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-06-20 17:30:50 -0300

    feat: use nodejs signals to process application exit successfully

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
 import { SetupServer } from './server';
 import config from 'config';
 
-// Self invocked function
+import logger from './logger';
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+// Self invocked function to start and exit app
 (async (): Promise<void> => {
-  const server = new SetupServer(config.get('App.port'));
-  await server.init();
-  server.start();
+  try {
+    const server = new SetupServer(config.get('App.port'));
+    await server.init();
+    server.start();
+
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+
+    exitSignals.map((signal) =>
+      process.on(signal, async () => {
+        try {
+          await server.close();
+          logger.info(`App exited with sucess. Signal: ${signal}`);
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          logger.error(`App exited with error: ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
+  } catch (error) {
+    logger.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
+  }
 })();
