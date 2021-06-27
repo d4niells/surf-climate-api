b1bffd8c3d6342c6c2454204403fb112f55a1a4f
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-06-27 12:56:50 -0300

    docs: add swagger to documentation surf climate api

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import './utils/module-alias';
 import express, { Application } from 'express';
 import expressPino from 'express-pino-logger';
 import { Server } from '@overnightjs/core';
+import swaggerUI from 'swagger-ui-express';
 import cors from 'cors';
+
+import { OpenApiValidator } from 'express-openapi-validator';
+import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types';
 
 import config from 'config';
 
@@ -14,6 +18,8 @@ import { ForecastController } from '@src/controllers/forecast';
 import { BeachesController } from '@src/controllers/beaches';
 import { UsersController } from '@src/controllers/users';
 
+import apiScheme from '@src/api.schema.json';
+
 export class SetupServer extends Server {
   constructor(private port: number = config.get('App.port')) {
     super();
@@ -21,6 +27,7 @@ export class SetupServer extends Server {
 
   public async init(): Promise<void> {
     this.setupExpress();
+    await this.docsSetup();
     this.setupControllers();
     await this.databaseSetup();
   }
@@ -41,6 +48,16 @@ export class SetupServer extends Server {
       beachesController,
       usersController,
     ]);
+  }
+
+  private async docsSetup(): Promise<void> {
+    this.app.use('/docs', swaggerUI.serve, swaggerUI.setup(apiScheme));
+
+    await new OpenApiValidator({
+      apiSpec: apiScheme as OpenAPIV3.Document,
+      validateRequests: true,
+      validateResponses: true,
+    }).install(this.app);
   }
 
   private async databaseSetup(): Promise<void> {
